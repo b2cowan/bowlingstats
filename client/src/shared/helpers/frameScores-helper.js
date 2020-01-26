a2c37ffcf724c3ab02a58a5349fa37e07249e801
Cowan <rcowan@sscinc.com>
2020-01-26 13:48:26 -0500

    made entering strikes case insensitive

--- client/src/shared/helpers/frameScores-helper.js
+++ client/src/shared/helpers/frameScores-helper.js
@@ -2,19 +2,19 @@
 export const frameScore = (
     frameNum,
     prevScore,
-    f1s1,
-    f1s2,
-    f1s3,
-    f2s1,
-    f2s2,
-    f3s1
+    f1s1 = "",
+    f1s2 = "",
+    f1s3 = "",
+    f2s1 = "",
+    f2s2 = "",
+    f3s1 = ""
 ) => {
     if (frameNum < 9) {
-        if (f1s1 === 'x') {
-            if (f2s1 === 'x' && f3s1 === 'x') {
+        if (f1s1.toLowerCase() === 'x') {
+            if (f2s1.toLowerCase() === 'x' && f3s1.toLowerCase() === 'x') {
                 return parseInt(prevScore) + 30;
             }
-            if (f2s1 === 'x' && f3s1 !== 'x') {
+            if (f2s1.toLowerCase() === 'x' && f3s1.toLowerCase() !== 'x') {
                 return parseInt(prevScore) + 20 + parseInt(f3s1);
             }
             if (f2s2 === '/') {
@@ -25,7 +25,7 @@ export const frameScore = (
             }
         }
         if (f1s2 === '/') {
-            if (f2s1 === 'x') {
+            if (f2s1.toLowerCase() === 'x') {
                 return parseInt(prevScore) + 20;
             }
             else {
@@ -38,11 +38,11 @@ export const frameScore = (
 
     }
     if (frameNum === 9) {
-        if (f1s1 === 'x') {
-            if (f2s1 === 'x' && f2s2 === 'x') {
+        if (f1s1.toLowerCase() === 'x') {
+            if (f2s1.toLowerCase() === 'x' && f2s2.toLowerCase() === 'x') {
                 return parseInt(prevScore) + 30;
             }
-            if (f2s1 === 'x' && f2s2 !== 'x') {
+            if (f2s1.toLowerCase() === 'x' && f2s2.toLowerCase() !== 'x') {
                 return parseInt(prevScore) + 20 + parseInt(f2s2);
             }
             if (f2s2 === '/') {
@@ -53,7 +53,7 @@ export const frameScore = (
             }
         }
         if (f1s2 === '/') {
-            if (f2s1 === 'x') {
+            if (f2s1.toLowerCase() === 'x') {
                 return parseInt(prevScore) + 20;
             }
             else {
@@ -65,9 +65,9 @@ export const frameScore = (
         }
     }
     if (frameNum === 10) {
-        if (f1s1 === 'x') {
-            if (f1s2 === 'x') {
-                if (f1s3 === 'x') {
+        if (f1s1.toLowerCase() === 'x') {
+            if (f1s2.toLowerCase() === 'x') {
+                if (f1s3.toLowerCase() === 'x') {
                     return parseInt(prevScore) + 30;
                 }
                 else {
@@ -83,7 +83,7 @@ export const frameScore = (
         }
         if (f1s2 === '/') {
 
-            if (f1s3 === 'x') {
+            if (f1s3.toLowerCase() === 'x') {
                 return parseInt(prevScore) + 20;
             }
             else {
